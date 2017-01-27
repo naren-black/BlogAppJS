fe0de5ffca148df33ada20755ec5517ed53bdb0f
Naren Nath <narenblue@gmail.com>
2017-01-27 13:14:32 +0000

    Style and finish Blog App

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
+var methodOverride = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 mongoose.connect("mongodb://localhost/blog_app");
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(express.static("public"));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
   title: String,
@@ -36,6 +40,7 @@ app.get("/blogs/new", function(req, res) {
 });
 
 app.post("/blogs", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
   Blog.create(req.body.blog, function(err,newBlog) {
     if(err) {
       res.render("new");
@@ -53,6 +58,45 @@ app.get("/blogs/:id", function(req,res) {
       res.render("show", {blogg: blogg});
     }
   })
+});
+
+app.get("/blogs/:id/edit", function(req, res) {
+  Blog.findById(req.params.id, function(err,blogy) {
+    if(err) {
+      res.redirect("/blogs");
+    } else {
+      res.render("edit", {blogy: blogy});
+    }
+  });
+});
+
+app.put("/blogs/:id", function(req, res) {
+  req.body.blog.body = req.sanitize(req.body.blog.body);
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog) {
+    if(err) {
+      console.log(err);
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
+});
+
+app.delete("/blogs/:id", function(req, res) {
+  // Blog.findById(req.params.id, function(err, blogi) {
+  //   if(err) {
+  //     res.redirect("/blogs/" + req.params.id);
+  //   }else {
+  //     blogi.remove();
+  //     res.redirect("/blogs");
+  //   }
+  // });
+  Blog.findByIdAndRemove(req.params.id, function(err) {
+    if(err) {
+      res.redirect("/blogs/" + req.params.id);
+    } else {
+      res.redirect("/blogs");
+    }
+  });
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
